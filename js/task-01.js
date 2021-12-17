a60c9ef86f4aeeea809889ab1af89f24cd67857a
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-18 01:02:13 +0200

    bugfix task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,6 @@
 const itemRef = document.querySelectorAll(".item");
 console.log(`Number of Categories: ${itemRef.length}`);
-const titleRef = document.querySelectorAll(".item");
-titleRef.forEach((title) => {
-  console.log(`Category: ${title.firstElementChild.textContent}`);
-  console.log(`Elements: ${title.lastElementChild.children.length}`);
+itemRef.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
