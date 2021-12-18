65488eea569cf413101380cc7fc677a078c3147a
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-18 23:57:55 +0200

    completed task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+}
